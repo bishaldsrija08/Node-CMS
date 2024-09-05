94ea7e08582775322b3d89958a3216c62685cb15
bishaldsrija08 <bishaldsrijal1@gmail.com>
2024-09-05 21:56:17 +0545

    footer added

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -1,12 +1,28 @@
+// const multer = require("multer");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./storage"); // cb(err,success)
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// module.exports = { multer, storage };
+
+
 const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./storage"); // cb(err,success)
+    cb(null, "storage"); // Removed './' for the correct relative path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-module.exports = { multer, storage };
+const upload = multer({ storage });
+
+module.exports = { upload };
